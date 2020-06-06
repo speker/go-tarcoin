744e734afe5f3a1e3824c8b6f3342e80e0141fb6
serkan peker <speker@gmail.com>
2020-06-07 00:41:52 +0300

    change port 60909

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -596,7 +596,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 30303,
+		Value: 60909,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
